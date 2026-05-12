6ad82988ad4eb8cc4c775077a58f47f880f16362
Mohammad Abujaradeh <mohammadabujaradeh@Mohammads-MacBook-Pro.local>
2026-05-12 14:51:42 -0500

    create LLM and Storage service

---
+++ app/services/llm_service.py
@@ -0,0 +1,31 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from app.config import Config
+
+class LLMService:
+    def __init__(self, vector_store):
+        self.llm = ChatOpenAI(
+            model="gpt-3.5-turbo",
+            temperature=0.7,
+            openai_api_key= Config.OPENAI_API_KEY
+        )
+
+        self.memory = ConversationBufferMemory(
+            memory_key = "chat_history",
+            return_messages = True
+        )
+
+        self.chain = ConversationalRetrievalChain(
+            llm = self.llm,
+            retriever = vector_store.as_retriever(),
+            memory = self.memory
+        )
+
+    def get_response(self, query):
+        try:
+            response = self.chain.run(query)
+            return response["answer"]
+        except Exception as e:
+            print(f" Error getting LLM response: {e}")
+            return "Sorry, I couldn't process your request at the moment."
---
+++ app/services/storage_service.py
@@ -0,0 +1,30 @@
+import boto3 
+from botocore.exceptions import ClientError
+from app.config import Config
+
+class StorageService:
+    def __init__(self):
+        self.s3 = boto3.client(
+            "s3",
+            aws_access_key_id = Config.AWS_ACCESS_KEY,
+            aws_secret_access_key = Config.AWS_SECRET_KEY
+        )
+
+        self.bucket_name = Config.AWS_BUCKET_NAME
+
+    def upload_file(self, fileObj, fileName):
+        try:
+            self.s3.upload_fileobj(fileObj, self.bucket_name, fileName)
+            return None;
+
+        except ClientError as e:
+            print(f"Error uploading file to S3: {e}")
+            return False;
+
+    def get_file(self, fileName):
+        try:
+            response = self.s3.get_object(Bucket=self.bucket_name, Key=fileName)
+            return response['Body'].read()
+        except ClientError as e:
+            print(f"Error retrieving file from S3: {e}")
+            return None
