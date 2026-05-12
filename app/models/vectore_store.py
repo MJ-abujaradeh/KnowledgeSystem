5905ad903a4c1338bbdef64934ae8567015ca2fc
Mohammad Abujaradeh <mohammadabujaradeh@Mohammads-MacBook-Pro.local>
2026-05-12 15:30:26 -0500

    Vector staore serivce;

---
+++ app/models/vectore_store.py
@@ -0,0 +1,20 @@
+import chromadb
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+from app.config import Config
+class VectoreStore:
+    def __init__(self, path):
+        self.embeddings = OpenAIEmbeddings(
+                model="text-embedding-3-small",
+                openai_api_key= Config.OPENAI_API_KEY
+        );
+        self.vector_store = Chroma(
+            persist_directory = path,
+            embedding_function = self.embeddings
+        )
+        
+    def add_documents(self, documents):
+        self.vector_store.add_documents(documents)
+
+    def similarity_search(self, query, k=4):
+        return self.vector_store.similarity_search(query, k=k)
